45eff97bc031af056b786ec30058824e3e301c4a
Kalam Pinjar <owaiskal57@gmail.com>
2024-07-17 00:43:29 +0530

    dark mode added ,finalized

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,5 +1,13 @@
-import prismadb from "@/lib/prismadb";
-import { redirect } from "next/navigation";
+import { getGraphRevenue } from "@/actions/get-graph-revenue";
+import { getTotalSales } from "@/actions/get-sales-count";
+import { getTotalProductsInStock } from "@/actions/get-stock-count";
+import { getTotalRevenue } from "@/actions/get-total-revenue";
+import Overview from "@/components/overview";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { formatter } from "@/lib/utils";
+import { CreditCard, DollarSign, Package } from "lucide-react";
 
 interface DashboardPageProps {
   params: {
@@ -8,17 +16,61 @@ interface DashboardPageProps {
 }
 
 const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
-  const store = await prismadb.store.findFirst({
-    where: {
-      id: params.storeId,
-    },
-  });
-
-  if (!store) {
-    redirect("/");
-  }
-
-  return <div>Active Store:{store?.name}</div>;
+  const totalRevenue = await getTotalRevenue(params.storeId);
+  const totalSales = await getTotalSales(params.storeId);
+  const totalProductsInStock = await getTotalProductsInStock(params.storeId);
+  const graphRevenue = await getGraphRevenue(params.storeId);
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <Heading title="Dashboard" description="Manage your store" />
+        <Separator />
+        <div className="gap-4 grid grid-cols-3">
+          <Card>
+            <CardHeader className="flex flex-row justify-between items-center space-y-2">
+              <CardTitle className="font-medium text-sm">
+                Total Revenue
+              </CardTitle>
+              <DollarSign className="w-4 h-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="font-bold text-2xl">
+                {formatter.format(totalRevenue)}
+              </div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row justify-between items-center space-y-2">
+              <CardTitle className="font-medium text-sm">Sales</CardTitle>
+              <CreditCard className="w-4 h-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="font-bold text-2xl">+{totalSales}</div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row justify-between items-center space-y-2">
+              <CardTitle className="font-medium text-sm">
+                Products In Stock
+              </CardTitle>
+              <Package className="w-4 h-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="font-bold text-2xl">{totalProductsInStock}</div>
+            </CardContent>
+          </Card>
+        </div>
+        <Card className="col-span-4">
+          <CardHeader>
+            <CardTitle>Overview</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Overview data={graphRevenue} />
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,7 @@ import StoreSwitcher from "./store-switcher";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb";
+import { ModeToggle } from "./theme-toggle";
 
 const Navbar = async () => {
   const { userId } = auth();
@@ -20,6 +21,7 @@ const Navbar = async () => {
         <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="flex items-center space-x-4 ml-auto">
+          <ModeToggle />
           <UserButton />
         </div>
       </div>
